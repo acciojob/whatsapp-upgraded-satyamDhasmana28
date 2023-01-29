e81433577fbf12d7bff7876faff1887a6b9ebb59
Your Name <satyam.dhasmanaa@gmail.com>
2023-01-29 15:15:02 +0530

    test 3

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -37,6 +37,11 @@ public class Message {
         this.timestamp = timestamp;
     }
 
+    public Message(int id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
     public Message() {
     }
 }
